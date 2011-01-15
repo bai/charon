21ebf28b0db1a7cd271f3957fb29efc23ff67e41
beawesomeinstead <beawesomeinstead@yahoo.com>
2011-01-15 22:34:04 +0200

    Added LoginTicket unit tests.

---
+++ test/unit/login_ticket_test.rb
@@ -0,0 +1,20 @@
+require_relative "../test_helper"
+
+# Most tests are in test/protocol. Tests here are outside of the protocol, but are necessary anyway.
+class LoginTicketTest < Test::Unit::TestCase
+  context "A ServiceTicket" do
+    setup do
+      @redis = Redis.new
+      @lt = LoginTicket.new("quentin")
+      @lt.save!(@redis)
+    end
+
+    should "be able to retrieve remaining time" do
+      assert_equal(300, @lt.remaining_time(@redis))
+    end
+
+    should "return a ticket" do
+      assert_not_nil @lt.ticket
+    end
+  end
+end
